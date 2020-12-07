1fb3ea3e48a697cd21b3bee9a582789c65a79424
David Granström <david@davidgranstrom.com>
2020-12-07 18:56:31 +0100

    Be able to pack an OSC message

--- src/message.lua
+++ src/message.lua
@@ -1,14 +1,66 @@
+local inspect = require'inspect'
+local Types = require'atomic'
 
--- function pack_values(types, ...)
---   local data = {''}
---   local values = {...}
---   for index, type in ipairs(types) do
---     local pack = pack_types[type]
---     if pack then
---       local value = values[index]
---       table.insert(data, pack(value))
---     end
---   end
---   return table.concat(data, '')
+local OSCMessage = {}
+-- OSCMessage.__index = {}
+-- function OSCMessage.new(address, ...)
+--   local self = setmetatable({}, OSCMessage)
+--   local args = {...}
+--   self.message = {
+--     address = address,
+--   }
 -- end
 
+local msg = {
+  address = "/foo/bar",
+  types = 'iTsf',
+  args = {
+    123,
+    'hello',
+    123.456,
+  }
+}
+
+local function add_to_packet(packet, type, value)
+  local pack = Types.pack[type]
+  if pack then
+    local buffer = pack(value)
+    assert(buffer, 'Error packing type ' .. type)
+    table.insert(packet, buffer)
+  else
+    print('Warning: Unrecognized type ' .. type)
+  end
+end
+
+function OSCMessage.pack(tbl)
+  assert(tbl.address, 'An OSC message must have an address.')
+  assert(tbl.types, 'An OSC message must have at least one type.')
+
+  local packet = {}
+  local address = tbl.address
+  local types = tbl.types
+
+  -- prefix if missing
+  if address:sub(1,1) ~= '/' then
+    address = '/' .. address
+  end
+
+  add_to_packet(packet, 's', address)
+  add_to_packet(packet, 's', ',' .. types)
+
+  local index = 1
+  types = types:gsub('[TFNI]', '')
+  for type in types:gmatch('.') do
+    if tbl.args then
+      local item = tbl.args[index]
+      add_to_packet(packet, type, item)
+      index = index + 1
+    end
+  end
+
+  packet = table.concat(packet, '')
+  return packet, #packet
+end
+
+function OSCMessage.unpack(data)
+end
