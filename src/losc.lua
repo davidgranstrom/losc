de3b5fcb638e9704a1ed37c1d91270efaedcf8e6
David Granström <david@davidgranstrom.com>
2020-12-29 15:06:26 +0100

    First sketch of plugin API

--- src/losc.lua
+++ src/losc.lua
@@ -10,8 +10,10 @@
 
 local Message = require'losc.message'
 local Bundle = require'losc.bundle'
+local Timetag = require'losc.timetag'
 
 local losc = {}
+losc.__index = losc
 
 --- Create a new Message.
 --
@@ -46,6 +48,58 @@ end
 -- @usage local bundle = losc.bundle_new(0.25, osc_msg, other_bundle)
 function losc.bundle_new(...)
   return Bundle.new(...)
+end
+
+--- Specify server/client plugin to use
+-- @param plug Table with server and client entries.
+function losc:use(plug)
+  if not self.plugin then
+    self.plugin = {}
+  end
+  if plug.now then
+    self.plugin.now = plug.now
+  end
+  if plug.client then
+    self.plugin.client = plug.client
+  end
+  if plug.server then
+    self.plugin.server = plug.server
+  end
+end
+
+--- Get a OSC timetag with the current timestamp.
+-- Will fall back to os.time() if not implemented by a plugin.
+function losc:now()
+  if self.plugin.now then
+    return self.plugin:now()
+  end
+  local now = os.time()
+  return Timetag.new_from_usec(now)
+end
+
+function losc:start(...)
+  if not self.plugin then
+    error('"start" must be implemented by a plugin.')
+  end
+  self.plugin.server:start(...)
+end
+
+function losc:stop(...)
+  if not self.plugin then
+    error('"stop" must be implemented by a plugin.')
+  end
+  self.plugin.server:stop(...)
+end
+
+function losc:send(...)
+  if not self.plugin then
+    error('"send" must be implemented by a plugin.')
+  end
+  self.plugin.client:send(...)
+end
+
+function losc:listen(addr, cb)
+
 end
 
 return losc
---
+++ src/losc/plugins/udp/libuv.lua
@@ -0,0 +1,52 @@
+local uv = require'luv'
+local Timetag = require'losc.timetag'
+
+local unpack = unpack or table.unpack
+local errors = {
+  not_running = 'UDP server not running.'
+}
+
+local M = {}
+M.__index = M
+
+function M:now()
+  local tv = uv.gettimeofday()
+  return Timetag.new_from_usec(unpack(tv))
+end
+
+function M:start(host, port)
+  host = host or "127.0.0.1"
+  port = port or 0
+  self.handle = uv.new_udp('inet')
+  assert(self.handle, 'Could not create UDP handle')
+  self.handle:bind(host, port, {reuseaddr=true})
+  self.handle:recv_start(function(err, data)
+    assert(not err, err)
+    if data then
+      print(data)
+      -- TODO: dispatch
+    end
+  end)
+  self.port = self.handle:getsockname().port
+  uv.run()
+end
+
+function M:stop()
+  assert(self.handle, errors.not_running)
+  self.handle:recv_stop()
+  if not self.handle:is_closing() then
+    self.handle:close()
+  end
+  uv.walk(uv.close)
+end
+
+function M:send(packet, options)
+  assert(self.handle, errors.not_running)
+  assert(packet, 'OSC packet is nil.')
+  local data = packet:pack()
+  local addr = options.address
+  local port = options.port
+  self.handle:udp_try_send(data, addr, port)
+end
+
+return M
