f653e5a71dee6e779db1fb64e37058fafbe8243b
David Granström <david@davidgranstrom.com>
2021-01-03 10:30:26 +0100

    Update opaque API

--- src/losc.lua
+++ src/losc.lua
@@ -27,79 +27,72 @@ function losc.message_new(...)
   return Message.new(...)
 end
 
---- Create a new Message from binary OSC data.
---
--- @param data Binary OSC data string.
--- @return Message object.
--- @see losc.message
--- @usage local message = losc.message_new_from_bytes(data)
-function losc.message_new_from_bytes(data)
-  return Message.new_from_bytes(data)
-end
-
 --- Create a new OSC bundle.
 --
 -- @param[opt] ... arguments.
 -- @return Bundle object.
 -- @see losc.bundle
 -- @usage local bundle = losc.bundle_new()
--- @usage local bundle = losc.bundle_new(0)
--- @usage local bundle = losc.bundle_new(0.25, osc_msg, osc_msg2)
--- @usage local bundle = losc.bundle_new(0.25, osc_msg, other_bundle)
+-- @usage local bundle = losc.bundle_new(Timetag.new())
+-- @usage local bundle = losc.bundle_new(Timetag.new_from_usec(os.time(), 0), osc_msg, osc_msg2)
+-- @usage local bundle = losc.bundle_new(Timetag.new_from_usec(os.time(), 0), osc_msg, other_bundle)
 function losc.bundle_new(...)
   return Bundle.new(...)
 end
 
---- Specify server/client plugin to use
--- @param plug Table with server and client entries.
-function losc:use(plug)
-  if not self.plugin then
-    self.plugin = {}
-  end
-  if plug.now then
-    self.plugin.now = plug.now
-  end
-  if plug.client then
-    self.plugin.client = plug.client
-  end
-  if plug.server then
-    self.plugin.server = plug.server
-  end
+--- Specify a plugin.
+-- @param plugin The plugin to use.
+function losc:use(plugin)
+  self.plugin = plugin
 end
 
 --- Get a OSC timetag with the current timestamp.
--- Will fall back to os.time() if not implemented by a plugin.
+-- Will fall back to os.time() if now() is not implemented by a plugin.
 function losc:now()
   if self.plugin.now then
     return self.plugin:now()
   end
-  local now = os.time()
-  return Timetag.new_from_usec(now)
+  return Timetag.new_from_usec(os.time(), 0)
 end
 
-function losc:start(...)
+--- Opens an OSC server.
+function losc:open(...)
   if not self.plugin then
-    error('"start" must be implemented by a plugin.')
+    error('"open" must be implemented by a plugin.')
   end
-  self.plugin.server:start(...)
+  self.plugin:start(...)
 end
 
-function losc:stop(...)
+--- Closes an OSC server.
+function losc:close(...)
   if not self.plugin then
-    error('"stop" must be implemented by a plugin.')
+    error('"close" must be implemented by a plugin.')
   end
-  self.plugin.server:stop(...)
+  self.plugin:stop(...)
 end
 
+--- Send an OSC packet.
 function losc:send(...)
   if not self.plugin then
     error('"send" must be implemented by a plugin.')
   end
-  self.plugin.client:send(...)
+  self.plugin:send(...)
 end
 
-function losc:listen(addr, cb)
+--- Add an OSC method.
+function losc:add_method(pattern, cb)
+  if not self.plugin then
+    error('"add_method" must be implemented by a plugin.')
+  end
+  self.plugin.methods[pattern] = cb
+end
 
+--- Remove an OSC method.
+function losc:remove_method(pattern)
+  if not self.plugin then
+    error('"remove_method" must be implemented by a plugin.')
+  end
+  self.plugin.methods[pattern] = nil
 end
 
 return losc
