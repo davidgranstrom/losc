5df3d9a773ab1e4ead643d7eadd6d96560510bc1
David Granström <david@davidgranstrom.com>
2021-01-15 14:02:43 +0100

    Update examples

--- examples/client.lua
+++ examples/client.lua
@@ -8,7 +8,7 @@ local plugin = require'losc.plugins.udp-socket'
 local udp = plugin.new { sendAddr = 'localhost', sendPort = 9000 }
 local osc = losc.new { plugin = udp }
 
-local message = osc.new_message {
+local message = losc.new_message {
   address = '/foo/bar',
   types = 'ifsb',
   123, 1.234, 'hi', 'blobdata'
--- examples/server.lua
+++ examples/server.lua
@@ -8,7 +8,7 @@ local plugin = require'losc.plugins.udp-socket'
 local udp = plugin.new { recvAddr = 'localhost', recvPort = 9000 }
 local osc = losc.new { plugin = udp }
 
-local function print(data)
+local function print_data(data)
   local msg = data.message
   print('address: ' .. msg.address, 'timestamp: ' .. data.timestamp)
   for index, argument in ipairs(msg) do
@@ -16,12 +16,12 @@ local function print(data)
   end
 end
 
-losc:add_handler('/test', function(data)
-  print(data)
+osc:add_handler('/test', function(data)
+  print_data(data)
 end)
 
-losc:add_handler('/param/{x,y,z}', function(data)
-  print(data)
+osc:add_handler('/param/{x,y,z}', function(data)
+  print_data(data)
 end)
 
-losc:open() -- blocking call
+osc:open() -- blocking call
